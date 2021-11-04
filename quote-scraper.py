e842a44d8d4d087c59d707fa056c2ed0cf2e79f1
andreitr <andrei.taraschuk@gmail.com>
2021-11-03 22:52:45 -0600

    Python scrape + quotes.

---
+++ quote-scraper.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL_STR = "https://www.goodreads.com/author/quotes/3137322.Fyodor_Dostoevsky?page="
+
+for x in range(100):
+  URL=URL_STR+str(x+1)
+  page = requests.get(URL)
+
+  soup = BeautifulSoup(page.content, "html.parser")
+  results = soup.find(class_="quotes")
+
+  quotes =results.find_all("div", class_="quote")
+
+  with open("fyodor.txt", "a") as f:
+    for quote in quotes:
+      quote_text = quote.find("div", class_="quoteText")
+      f.write(str(quote_text).split("<br/>")[0].split("<div class=\"quoteText\">")[1].strip())
+      f.write('\n\n')
