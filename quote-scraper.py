dd7f2faa71b52d174b78568543b5a713694d741b
andreitr <andrei.taraschuk@gmail.com>
2022-10-06 20:10:50 -0600

    Misc

--- quote-scraper.py
+++ quote-scraper.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL_STR = "https://www.goodreads.com/author/quotes/3137322.Fyodor_Dostoevsky?page="
+URL_STR = "https://www.goodreads.com/author/quotes/34583.Vincent_van_Gogh?page="
 
 for x in range(100):
   URL=URL_STR+str(x+1)
@@ -12,7 +12,7 @@ for x in range(100):
 
   quotes =results.find_all("div", class_="quote")
 
-  with open("fyodor.txt", "a") as f:
+  with open("quote.txt", "a") as f:
     for quote in quotes:
       quote_text = quote.find("div", class_="quoteText")
       f.write(str(quote_text).split("<br/>")[0].split("<div class=\"quoteText\">")[1].strip())
